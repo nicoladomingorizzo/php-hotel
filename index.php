c6061d5938c6049704c6f5e1928119763a987995
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-09-29 17:45:10 +0200

    printed all values inside the table

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
+<?php
+
+$title = 'PHP Hotels'
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title><?php echo $title ?></title>
 </head>
-<body>
-    <h1>PHP Hotels</h1>
+<body class='text-center'>
 
+    <h1 class='my-5 p-2'>
+        <?php
+        echo $title
+        ?>
+    </h1>
+    
     <?php
 
     $hotels = [
@@ -48,13 +60,38 @@
         ],
     ];
 
-    foreach ($hotels as $hotel) {
-        foreach ($hotel as $key => $value) {
-            echo $key . ": $value " . '<br>';
-        }
-        echo '<br>';
-    }
     ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nome Hotel</th>
+                <th scope="col">Hotel Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope='col'>Distanza dal centro (Km)</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($hotels as $hotel) {
+            echo '<tr>';
+            // Proprietà dell'hotel (name, description, parking, vote, distance_to_center)
+            foreach ($hotel as $key => $value) {
+                // Gestione per 'parking'
+                if ($key === 'parking') {
+                    $display_value = $value ? 'Sì' : 'No';
+                } else {
+                    $display_value = $value;
+                }
 
+                echo '<td>' . $display_value . '</td>';
+            }
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
